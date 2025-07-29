25e4dad0bba1d9b5fc1cb1b1173ddc8af6fbf424
ElectroBoxUA <84919099+ElektroBOXua@users.noreply.github.com>
2025-07-29 16:38:51 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+#MISRA C Checker Script
+This script provides a simple way to set up cppcheck with MISRA C 2023 addons and run checks on your C source files.
+
+#Usage
+##Setup
+To set up the environment, including cloning cppcheck and downloading the necessary MISRA rule texts:
+
+./misra.sh setup
+
+##Check
+To run a MISRA check on a C source or header file:
+
+./misra.sh check <target_file.c> or ./misra.sh check <target_file.h>
+
+(Replace <target_file.*> with the path to your C source/header file.)
+
+This script leverages cppcheck for static analysis and a Python script (misra.py) from the cppcheck addons for MISRA rule checking.
---
+++ misra.sh
@@ -0,0 +1,68 @@
+function misra_setup() {
+	# go to the same folder where script is running
+	cd "$(dirname "$0")" || exit 1
+
+	git clone -n --depth=1 --filter=tree:0 \
+		https://github.com/danmar/cppcheck.git cppcheck &> /dev/null
+	cd cppcheck
+	git sparse-checkout set --no-cone addons/
+	git checkout &> /dev/null
+
+	# Download the MISRA C 2023 headlines file
+	RAW_FILE_URL="https://gitlab.com/MISRA/MISRA-C/MISRA-C-2012/tools/-/raw/main/misra_c_2023__headlines_for_cppcheck.txt"
+	DOWNLOAD_PATH="./addons/misra_c_2023__headlines_for_cppcheck.txt"
+
+	# Check if the file already exists before attempting to download
+	if [ -f "$DOWNLOAD_PATH" ]; then
+		echo "MISRA C 2023 headlines file already exists: $DOWNLOAD_PATH (skipping download)"
+	else
+		echo "Downloading MISRA C 2023 headlines file..."
+		if ! curl -sSL "$RAW_FILE_URL" -o "$DOWNLOAD_PATH"; then
+			echo "Error: Failed to download $RAW_FILE_URL" >&2
+			exit 1
+		fi
+		echo "Download complete: $DOWNLOAD_PATH"
+	fi
+}
+function misra_check() {
+	MISRA_PATH="$(dirname "$0")""/cppcheck/addons/"
+	TARGET="$1"
+
+	function cleanup() {
+		# Remove garbage
+		rm "${TARGET}.ctu"*
+		rm "${TARGET}.dump"*
+	}
+
+	# Set a trap to call the cleanup function on script exit (EXIT signal)
+	trap cleanup EXIT
+
+	# Run check
+	cppcheck --dump --std=c89 "${TARGET}"
+	python "${MISRA_PATH}/misra.py" "${TARGET}.dump" \
+	  --rule-texts="${MISRA_PATH}/misra_c_2023__headlines_for_cppcheck.txt"
+
+	# Check if misra checks have passed
+	if [ $? -eq 0 ]; then
+		echo "MISRA check completed successfully for ${TARGET}."
+	else
+		echo "Error: MISRA check failed for ${TARGET}. Exit code: $?" >&2
+		exit 1 # Exit the script with an error if the MISRA check fails
+	fi
+}
+
+# --- Main Logic ---
+case "$1" in
+	setup)
+		misra_setup
+		;;
+	check)
+		shift
+		misra_check "$@"
+		;;
+	*)
+		echo "Usage: $0 {setup|check <target_file.*>}"
+		exit 1
+		;;
+esac
+
